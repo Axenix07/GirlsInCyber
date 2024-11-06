a59072b7cdd39754cd2e4975154580dbea882ddd
Axenix07 <gardaw1@gmail.com>
2024-11-05 21:07:49 -1000

    initial almost done lol

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,9 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>AFCEDS</title>
-        <meta
-            name="description"
-            content="Air Force Center for Electronic Distribution of Systems (AFCEDS) is a centralized tool for users to download commercial software products, and organically developed solutions."
-          />
-        <link rel="icon" href="/AFLCMC.png" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
